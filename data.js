ac9b3a9c34242075f1a9480e5e69cc1d222fab20
Jorge Yau <jyau01@citymail.cuny.edu>
2015-05-03 23:34:56 -0400

    Added basic project files

---
+++ data.js
@@ -0,0 +1,12 @@
+/*!
+ * nba-seasonal-wins
+ * MIT License (c) 2015
+ * https://github.com/codenameyau/nba-seasonal-wins
+ *
+ * Description:
+ * Run this nodejs script to grab and clean the seasonal team data.
+ */
+'use strict';
+
+API_ENDPOINT = 'https://erikberg.com/nba/team-stats/';
+API_FORMAT = '.json';
