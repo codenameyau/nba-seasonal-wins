20f0140540d411ee6ffc10e604306b46fb10aa72
Jorge Yau <jyau01@citymail.cuny.edu>
2015-05-04 00:12:04 -0400

    Added script to pull data from xmlstats API

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # nba-seasonal-wins
-Data visualization of the nba bandwagon
+Data visualization of the nba bandwagon.
+
+### Pull Data from API
+
+Export the environment variable `XMLSTATS_API_KEY`, like so:
+
+```bash
+export XMLSTATS_API_KEY="xxxxx-xxxx-xxxx"
+```
--- data.js
+++ data.js
@@ -8,5 +8,52 @@
  */
 'use strict';
 
-API_ENDPOINT = 'https://erikberg.com/nba/team-stats/';
-API_FORMAT = '.json';
+
+/***************************************************************
+* Dependencies
+***************************************************************/
+var request = require('request');
+
+
+/***************************************************************
+* Globals
+***************************************************************/
+var API_ENDPOINT = 'https://erikberg.com/nba/team-stats/';
+var API_FORMAT = '.json';
+
+// Create an environment variable with your API key.
+var ACCESS_KEY = process.env.XMLSTATS_API_KEY;
+var USER_AGENT = 'NBAStatsBot/6.9';
+
+
+/***************************************************************
+* Functions
+***************************************************************/
+var getUrl = function(resource) {
+  return API_ENDPOINT + resource + API_FORMAT;
+};
+
+var getOptions = function(resourceURL) {
+  return {
+    url: resourceURL,
+    headers: {
+      'Authorization': 'Bearer ' + ACCESS_KEY,
+      'User-Agent': USER_AGENT
+    }
+  };
+};
+
+var requestGetStandings = function() {
+  var resource = 'standings';
+  var options = getOptions(getUrl(resource));
+
+  // On hiatus until I get an API KEY.
+  request.get(options, function(err, res) {
+    console.log(res);
+    if (!err && res.statusCode === 200) {
+      console.log('Yay!');
+    }
+  });
+};
+
+requestGetStandings();
